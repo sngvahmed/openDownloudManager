a9c83c0be601e04a74c22a1ee97453b22bb4bade
ahmed <ahmednasser1993@gmail.com>
2015-02-22 01:23:11 +0200

    fixing importing of project

--- src/main/java/org/downloadManger/Main.java
+++ src/main/java/org/downloadManger/Main.java
@@ -7,9 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext(
 				"applicationContext.xml");
-
 		app.getBean("Home");
-
 		app.close();
 	}
 
